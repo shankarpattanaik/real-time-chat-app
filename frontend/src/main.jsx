703c5d2976383ab5ad8640604dd2073207162e67
shankarpattanaik <shankarpattanaik2000@gmail.com>
2026-03-12 09:37:56 +0530

    add server URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
-export const serverUrl = "http://localhost:8000";
+// export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://real-time-chat-app-18q3.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
